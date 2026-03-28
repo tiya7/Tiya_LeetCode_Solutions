91dbb7e2c1386fb4141187762e23f81dc714b555
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-03-29 00:13:34 +0530

    8) Remove Element.cpp

--- LeetCode_Solutions/Easy/8) Remove Element.cpp
+++ LeetCode_Solutions/Easy/8) Remove Element.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int k = 0; // position for next valid element
+        int k = 0;
         
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != val) {
                 nums[k] = nums[i];
                 k++;
             }
-        }
-        
+        }      
         return k;
     }
 };
