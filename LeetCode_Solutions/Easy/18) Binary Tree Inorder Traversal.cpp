bbbea95d8023a33afe009c55053a74d403cb595e
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-04-12 23:33:55 +0530

    Binary Tree Inorder Traversal

---
+++ LeetCode_Solutions/Easy/18) Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void solve(TreeNode* root, vector<int>& ans) {
+        if (!root) return;
+        solve(root->left, ans);
+        ans.push_back(root->val);
+        solve(root->right, ans);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        solve(root, ans);
+        return ans;
+    }
+};
