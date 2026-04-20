ed46d07210eca9f25c3a4a5fd897ee3ccf4a50c8
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-04-20 22:57:54 +0530

    3 Sum Closest

--- LeetCode_Solutions/Medium/12) 3 Sum Closest.cpp
+++ LeetCode_Solutions/Medium/12) 3 Sum Closest.cpp
@@ -1,1 +1,26 @@
-class Solution { public: int threeSumClosest(vector<int>& nums, int target) { } };
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int closest = nums[0] + nums[1] + nums[2];
+
+        for (int i = 0; i < n - 2; i++) {
+            int l = i + 1, r = n - 1;
+
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if (abs(target - sum) < abs(target - closest)) {
+                    closest = sum;
+                }
+
+                if (sum < target) l++;
+                else if (sum > target) r--;
+                else return sum;
+            }
+        }
+
+        return closest;
+    }
+};
