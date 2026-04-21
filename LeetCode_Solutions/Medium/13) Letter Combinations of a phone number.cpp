0e1211e3fc1a585e6adc6c63cbb2514bc88b3f7d
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-04-21 22:50:53 +0530

    Letter Combinations of a phone number

--- LeetCode_Solutions/Medium/13) Letter Combinations of a phone number.cpp
+++ LeetCode_Solutions/Medium/13) Letter Combinations of a phone number.cpp
@@ -1,1 +1,33 @@
-.
+class Solution {
+public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
+        
+        vector<string> mapping = {
+            "", "", "abc", "def", "ghi", 
+            "jkl", "mno", "pqrs", "tuv", "wxyz"
+        };
+        
+        vector<string> result;
+        string current = "";
+        
+        backtrack(digits, 0, mapping, current, result);
+        return result;
+    }
+    
+    void backtrack(string &digits, int index, vector<string> &mapping, 
+                   string &current, vector<string> &result) {
+        if (index == digits.size()) {
+            result.push_back(current);
+            return;
+        }
+        
+        string letters = mapping[digits[index] - '0'];
+        
+        for (char ch : letters) {
+            current.push_back(ch);
+            backtrack(digits, index + 1, mapping, current, result);
+            current.pop_back();
+        }
+    }
+};
