aa7561c2172a563391056ff4bc661415dc3dafb0
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-04-24 21:05:13 +0530

    Furthest point from origin

--- LeetCode_Solutions/Easy/23) Furthest point from origin.cpp
+++ LeetCode_Solutions/Easy/23) Furthest point from origin.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int furthestDistanceFromOrigin(string moves) {
-        
+        int l = 0, r = 0, blank = 0;
+
+        for (char c : moves) {
+            if (c == 'L') l++;
+            else if (c == 'R') r++;
+            else blank++;
+        }
+
+        return abs(l - r) + blank;
     }
 };
