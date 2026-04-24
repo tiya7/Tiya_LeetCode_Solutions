f95567208066ac0c6b412efce2d7cb646a04e696
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-04-24 08:50:20 +0530

    Generate Parantheses

--- LeetCode_Solutions/Medium/15) Generate Parantheses.cpp
+++ LeetCode_Solutions/Medium/15) Generate Parantheses.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
+    void generate(int open, int close, string s, vector<string>& ans) {
+        if (open == 0 && close == 0) {
+            ans.push_back(s);
+            return;
+        }
+
+        if (open > 0) {
+            generate(open - 1, close, s + "(", ans);
+        }
+
+        if (close > open) {
+            generate(open, close - 1, s + ")", ans);
+        }
+    }
+
     vector<string> generateParenthesis(int n) {
-        
+        vector<string> ans;
+        generate(n, n, "", ans);
+        return ans;
     }
 };
