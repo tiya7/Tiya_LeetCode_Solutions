276b8b3348110004a98c1ab68e0c3a4fa61cfea7
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-05-04 23:08:25 +0530

    Combination Sum II

--- LeetCode_Solutions/Medium/23) Combination Sum II.cpp
+++ LeetCode_Solutions/Medium/23) Combination Sum II.cpp
@@ -1,6 +1,38 @@
 class Solution {
 public:
+    void solve(int index, vector<int>& candidates, int target,
+               vector<int>& temp, vector<vector<int>>& ans) {
+
+        if (target == 0) {
+            ans.push_back(temp);
+            return;
+        }
+
+        for (int i = index; i < candidates.size(); i++) {
+            if (i > index && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+
+            if (candidates[i] > target) {
+                break;
+            }
+
+            temp.push_back(candidates[i]);
+
+            solve(i + 1, candidates, target - candidates[i], temp, ans);
+
+            temp.pop_back();
+        }
+    }
+
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        
+        vector<vector<int>> ans;
+        vector<int> temp;
+
+        sort(candidates.begin(), candidates.end());
+
+        solve(0, candidates, target, temp, ans);
+
+        return ans;
     }
 };
