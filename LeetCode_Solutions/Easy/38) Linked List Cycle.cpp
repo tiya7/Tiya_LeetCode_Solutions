94148fe01ab3b8b222089b7f83b03762573baa18
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-05-18 23:01:29 +0530

    Linked List Cycle

--- LeetCode_Solutions/Easy/38) Linked List Cycle.cpp
+++ LeetCode_Solutions/Easy/38) Linked List Cycle.cpp
@@ -1,25 +1,12 @@
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
 
-        while (fast && fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
-
-            if (slow == fast) {
-                ListNode* temp = head;
-
-                while (temp != slow) {
-                    temp = temp->next;
-                    slow = slow->next;
-                }
-
-                return true;
-            }
+        for (int num : nums) {
+            ans ^= num;
         }
 
-        return false;
+        return ans;
     }
 };
