afa722a4092cc996c324386a167430cd835116d5
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-05-23 22:44:53 +0530

    Excel Sheet Column Title

--- LeetCode_Solutions/Easy/43) Excel Sheet Column Title.cpp
+++ LeetCode_Solutions/Easy/43) Excel Sheet Column Title.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     string convertToTitle(int columnNumber) {
-        
+        string ans = "";
+
+        while (columnNumber > 0) {
+            columnNumber--;
+
+            char ch = 'A' + (columnNumber % 26);
+
+            ans = ch + ans;
+
+            columnNumber = columnNumber / 26;
+        }
+
+        return ans;
     }
 };
