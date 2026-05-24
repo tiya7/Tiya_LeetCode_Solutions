f0a81257fbc97533ad133d2cd0e81fcd227b7ae2
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-05-24 18:04:16 +0530

    Majority Element

--- LeetCode_Solutions/Easy/44) Majority Element.cpp
+++ LeetCode_Solutions/Easy/44) Majority Element.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        
+        int count = 0;
+        int ans = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                ans = num;
+            }
+
+            if (num == ans) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return ans;
     }
 };
