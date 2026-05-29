8a9659833dd19cd40cfe5ec9ba6f91e1df00b940
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-05-29 21:26:03 +0530

    Removed Linked List Elements

--- LeetCode_Solutions/Easy/49) Removed Linked List Elements.cpp
+++ LeetCode_Solutions/Easy/49) Removed Linked List Elements.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        
+        while (head != NULL && head->val == val) {
+            head = head->next;
+        }
+
+        ListNode* curr = head;
+
+        while (curr != NULL && curr->next != NULL) {
+            if (curr->next->val == val) {
+                curr->next = curr->next->next;
+            } else {
+                curr = curr->next;
+            }
+        }
+
+        return head;
     }
 };
