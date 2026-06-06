72d92a23f77540e339563631f0231d249aa7c51e
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-06-06 22:56:39 +0530

    Spiral Matrix II

--- LeetCode_Solutions/Medium/36) Spiral Matrix II.cpp
+++ LeetCode_Solutions/Medium/36) Spiral Matrix II.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        
+        vector<vector<int>> ans(n, vector<int>(n));
+
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int num = 1;
+
+        while (top <= bottom && left <= right) {
+
+            for (int i = left; i <= right; i++)
+                ans[top][i] = num++;
+            top++;
+
+            for (int i = top; i <= bottom; i++)
+                ans[i][right] = num++;
+            right--;
+
+            for (int i = right; i >= left; i--)
+                ans[bottom][i] = num++;
+            bottom--;
+
+            for (int i = bottom; i >= top; i--)
+                ans[i][left] = num++;
+            left++;
+        }
+
+        return ans;
     }
 };
