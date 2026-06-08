07b4b9c392e58f3789460d4803d5d497c527e8eb
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-06-08 19:24:38 +0530

    Rotate List

--- LeetCode_Solutions/Medium/37) Rotate List.cpp
+++ LeetCode_Solutions/Medium/37) Rotate List.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        
+        if (!head || !head->next || k == 0)
+            return head;
+
+        int n = 1;
+        ListNode* tail = head;
+
+        while (tail->next) {
+            tail = tail->next;
+            n++;
+        }
+
+        k = k % n;
+        if (k == 0)
+            return head;
+
+        tail->next = head;
+
+        int steps = n - k;
+        ListNode* newTail = head;
+
+        for (int i = 1; i < steps; i++)
+            newTail = newTail->next;
+
+        ListNode* newHead = newTail->next;
+        newTail->next = NULL;
+
+        return newHead;
     }
 };
