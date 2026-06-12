e9edae0998124a6400158d58a985cb364a3bbb06
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-06-12 18:58:41 +0530

    Simplify Path

--- LeetCode_Solutions/Medium/41) Simplify Path.cpp
+++ LeetCode_Solutions/Medium/41) Simplify Path.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        
+        vector<string> st;
+        string temp;
+
+        for (int i = 0; i <= path.size(); i++) {
+            if (i == path.size() || path[i] == '/') {
+                if (temp == "..") {
+                    if (!st.empty())
+                        st.pop_back();
+                } 
+                else if (temp != "" && temp != ".") {
+                    st.push_back(temp);
+                }
+                temp = "";
+            } 
+            else {
+                temp += path[i];
+            }
+        }
+
+        string ans = "";
+
+        for (string s : st) {
+            ans += "/" + s;
+        }
+
+        return ans == "" ? "/" : ans;
     }
 };
