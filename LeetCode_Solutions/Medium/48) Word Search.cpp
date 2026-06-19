d54a5e2bddd742c78aec0cf8aff82261cd6f819b
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-06-19 22:57:26 +0530

    Word Search

--- LeetCode_Solutions/Medium/48) Word Search.cpp
+++ LeetCode_Solutions/Medium/48) Word Search.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
+    bool dfs(vector<vector<char>>& board, string word, int i, int j, int k) {
+        if(k == word.size()) return true;
+
+        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] != word[k])
+            return false;
+
+        char ch = board[i][j];
+        board[i][j] = '#';
+
+        bool ans = dfs(board, word, i+1, j, k+1) ||
+                   dfs(board, word, i-1, j, k+1) ||
+                   dfs(board, word, i, j+1, k+1) ||
+                   dfs(board, word, i, j-1, k+1);
+
+        board[i][j] = ch;
+
+        return ans;
+    }
+
     bool exist(vector<vector<char>>& board, string word) {
-        
+        for(int i = 0; i < board.size(); i++) {
+            for(int j = 0; j < board[0].size(); j++) {
+                if(dfs(board, word, i, j, 0))
+                    return true;
+            }
+        }
+        return false;
     }
 };
