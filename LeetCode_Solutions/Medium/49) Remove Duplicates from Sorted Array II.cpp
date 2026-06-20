b2d412533877f5483491f02167b8f31a8b1dfc84
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-06-20 21:19:01 +0530

    Remove Duplicates from Sorted Array II

--- LeetCode_Solutions/Medium/49) Remove Duplicates from Sorted Array II.cpp
+++ LeetCode_Solutions/Medium/49) Remove Duplicates from Sorted Array II.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
+        int i = 0;
+        for(int x : nums){
+            if(i < 2 || x != nums[i-2]){
+                nums[i++] = x;
+            }
+        }
+        return i;
     }
 };
