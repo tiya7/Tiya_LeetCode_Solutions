ffa19348846458a345659fafe9bec7c6c18eed84
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-06-22 21:52:52 +0530

    Remove Duplicates from Sorted List II

--- LeetCode_Solutions/Medium/51) Remove Duplicates from Sorted List II.cpp
+++ LeetCode_Solutions/Medium/51) Remove Duplicates from Sorted List II.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* prev = &dummy;
+
+        while(head){
+            if(head->next && head->val == head->next->val){
+                while(head->next && head->val == head->next->val)
+                    head = head->next;
+                prev->next = head->next;
+            }
+            else{
+                prev = prev->next;
+            }
+            head = head->next;
+        }
+
+        return dummy.next;
     }
 };
