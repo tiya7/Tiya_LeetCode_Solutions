b96f4e4dbfde1b8ccd62dd524999f28f15a8dad9
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-06-23 22:09:18 +0530

    Partition List

--- LeetCode_Solutions/Medium/52) Partition List.cpp
+++ LeetCode_Solutions/Medium/52) Partition List.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        
+        ListNode small(0), large(0);
+        ListNode* s = &small;
+        ListNode* l = &large;
+
+        while(head){
+            if(head->val < x){
+                s->next = head;
+                s = s->next;
+            }
+            else{
+                l->next = head;
+                l = l->next;
+            }
+            head = head->next;
+        }
+
+        l->next = nullptr;
+        s->next = large.next;
+
+        return small.next;
     }
 };
