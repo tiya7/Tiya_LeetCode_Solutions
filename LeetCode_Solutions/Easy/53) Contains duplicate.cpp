24305370b2c93e167ce0ab26e15771364e2fe1fd
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-06-25 22:49:20 +0530

    Contains duplicate

--- LeetCode_Solutions/Easy/53) Contains duplicate.cpp
+++ LeetCode_Solutions/Easy/53) Contains duplicate.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        
+        unordered_set<int> s;
+
+        for(int x : nums){
+            if(s.count(x))
+                return true;
+            s.insert(x);
+        }
+
+        return false;
     }
 };
