15a40b93845bc082068c7c6342e89ab8432b6926
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-06-26 22:45:05 +0530

    Subsets II

--- LeetCode_Solutions/Medium/54) Subsets II.cpp
+++ LeetCode_Solutions/Medium/54) Subsets II.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        
+        vector<vector<int>> ans;
+        vector<int> temp;
+
+        sort(nums.begin(), nums.end());
+
+        function<void(int)> solve = [&](int i){
+            ans.push_back(temp);
+
+            for(int j = i; j < nums.size(); j++){
+                if(j > i && nums[j] == nums[j-1])
+                    continue;
+
+                temp.push_back(nums[j]);
+                solve(j + 1);
+                temp.pop_back();
+            }
+        };
+
+        solve(0);
+
+        return ans;
     }
 };
