61a53bc13a4609582d07b40474234c9c9cef2307
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-06-29 22:31:35 +0530

    Unique Binary Search Trees II

--- LeetCode_Solutions/Medium/58)Unique Binary Search Trees II.cpp
+++ LeetCode_Solutions/Medium/58)Unique Binary Search Trees II.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
+    vector<TreeNode*> solve(int start, int end) {
+        vector<TreeNode*> ans;
+        if(start > end) {
+            ans.push_back(nullptr);
+            return ans;
+        }
+
+        for(int i = start; i <= end; i++) {
+            vector<TreeNode*> left = solve(start, i - 1);
+            vector<TreeNode*> right = solve(i + 1, end);
+
+            for(auto l : left) {
+                for(auto r : right) {
+                    TreeNode* root = new TreeNode(i);
+                    root->left = l;
+                    root->right = r;
+                    ans.push_back(root);
+                }
+            }
+        }
+
+        return ans;
+    }
+
     vector<TreeNode*> generateTrees(int n) {
-        
+        return solve(1, n);
     }
 };
