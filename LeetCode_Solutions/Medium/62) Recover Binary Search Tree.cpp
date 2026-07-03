0c4af614d575c11d7efd9a2a0f1219e7ec15a31c
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-04 01:49:33 +0530

    Recover Binary Search Tree

--- LeetCode_Solutions/Medium/62) Recover Binary Search Tree.cpp
+++ LeetCode_Solutions/Medium/62) Recover Binary Search Tree.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
+    TreeNode *first = nullptr, *second = nullptr, *prev = nullptr;
+
+    void inorder(TreeNode* root) {
+        if (!root) return;
+        inorder(root->left);
+        if (prev && prev->val > root->val) {
+            if (!first) first = prev;
+            second = root;
+        }
+        prev = root;
+        inorder(root->right);
+    }
+
     void recoverTree(TreeNode* root) {
-        
+        inorder(root);
+        swap(first->val, second->val);
     }
 };
