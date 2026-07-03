43972ff9e6657a1f2b32d3cad4a9416755df9091
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-03 23:58:18 +0530

    Validate Binary Search Tree

--- LeetCode_Solutions/Medium/61) Validate Binary Search Tree.cpp
+++ LeetCode_Solutions/Medium/61) Validate Binary Search Tree.cpp
@@ -1,11 +1,12 @@
 class Solution {
-
 public:
+    bool check(TreeNode* node, long long low, long long high) {
+        if (!node) return true;
+        if (node->val <= low || node->val >= high) return false;
+        return check(node->left, low, node->val) && check(node->right, node->val, high);
+    }
 
-    bool isValidBST(TreeNode* root) {
-
-        
-
-    }
-
+    bool isValidBST(TreeNode* root) {
+        return check(root, LLONG_MIN, LLONG_MAX);
+    }
 };
