0ac581837ab8a3542c08c13f9803fcef671c8c08
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-04 22:59:46 +0530

    Binary Tree Level Order Traversal

--- LeetCode_Solutions/Medium/63) Binary Tree Level Order Traversal.cpp
+++ LeetCode_Solutions/Medium/63) Binary Tree Level Order Traversal.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        
+        vector<vector<int>> ans;
+        if (!root) return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int sz = q.size();
+            vector<int> level;
+
+            while (sz--) {
+                TreeNode* node = q.front();
+                q.pop();
+                level.push_back(node->val);
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            ans.push_back(level);
+        }
+
+        return ans;
     }
 };
