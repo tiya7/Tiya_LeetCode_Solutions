e6543c123a7ed980157fd2d12d75de56ad2833d3
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-06 22:59:49 +0530

    Construct Binary Tree from Inorder and Preorder Traversal

--- LeetCode_Solutions/Medium/65) Construct Binary Tree from Inorder and Preorder Traversal.cpp
+++ LeetCode_Solutions/Medium/65) Construct Binary Tree from Inorder and Preorder Traversal.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
+    unordered_map<int, int> mp;
+    int idx = 0;
+
+    TreeNode* solve(vector<int>& preorder, int l, int r) {
+        if (l > r) return nullptr;
+
+        TreeNode* root = new TreeNode(preorder[idx++]);
+        int mid = mp[root->val];
+
+        root->left = solve(preorder, l, mid - 1);
+        root->right = solve(preorder, mid + 1, r);
+
+        return root;
+    }
+
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        
+        for (int i = 0; i < inorder.size(); i++)
+            mp[inorder[i]] = i;
+        return solve(preorder, 0, inorder.size() - 1);
     }
 };
