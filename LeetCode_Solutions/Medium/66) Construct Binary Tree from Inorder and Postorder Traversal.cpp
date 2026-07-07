3fae0926e6dc6c7297961a3e7e563df362a9627c
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-07 22:32:53 +0530

    Implement binary tree construction from traversals
    Added a helper function to construct a binary tree from inorder and postorder traversal arrays.

--- LeetCode_Solutions/Medium/66) Construct Binary Tree from Inorder and Postorder Traversal.cpp
+++ LeetCode_Solutions/Medium/66) Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
+    unordered_map<int, int> mp;
+    int idx;
+
+    TreeNode* solve(vector<int>& inorder, vector<int>& postorder, int l, int r) {
+        if (l > r) return nullptr;
+
+        TreeNode* root = new TreeNode(postorder[idx--]);
+        int mid = mp[root->val];
+
+        root->right = solve(inorder, postorder, mid + 1, r);
+        root->left = solve(inorder, postorder, l, mid - 1);
+
+        return root;
+    }
+
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        
+        for (int i = 0; i < inorder.size(); i++)
+            mp[inorder[i]] = i;
+        idx = postorder.size() - 1;
+        return solve(inorder, postorder, 0, inorder.size() - 1);
     }
 };
