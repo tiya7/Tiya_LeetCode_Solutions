144b2e4ee0b04673b67f5f6ff7cfcd8449b8693f
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-09 23:04:22 +0530

    Convert sorted list to binary search tree

--- LeetCode_Solutions/Medium/68) Convert sorted list to binary search tree.cpp
+++ LeetCode_Solutions/Medium/68) Convert sorted list to binary search tree.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
+    TreeNode* build(vector<int>& nums, int left, int right) {
+        if (left > right)
+            return NULL;
+
+        int mid = (left + right) / 2;
+
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = build(nums, left, mid - 1);
+        root->right = build(nums, mid + 1, right);
+
+        return root;
+    }
+
     TreeNode* sortedListToBST(ListNode* head) {
-        
+        vector<int> nums;
+
+        while (head != NULL) {
+            nums.push_back(head->val);
+            head = head->next;
+        }
+
+        return build(nums, 0, nums.size() - 1);
     }
 };
