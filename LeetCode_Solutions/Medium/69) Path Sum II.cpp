2baceedce05ec0ac930e29f092293fea2c52327f
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-10 22:45:46 +0530

    Path Sum II

--- LeetCode_Solutions/Medium/69) Path Sum II.cpp
+++ LeetCode_Solutions/Medium/69) Path Sum II.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
+    vector<vector<int>> ans;
+    vector<int> path;
+
+    void dfs(TreeNode* root, int target) {
+        if (!root) return;
+
+        path.push_back(root->val);
+        target -= root->val;
+
+        if (!root->left && !root->right && target == 0)
+            ans.push_back(path);
+
+        dfs(root->left, target);
+        dfs(root->right, target);
+
+        path.pop_back();
+    }
+
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        
+        dfs(root, targetSum);
+        return ans;
     }
 };
