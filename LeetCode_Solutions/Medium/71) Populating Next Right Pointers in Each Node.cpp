2a9f1855dd5eac1d7260a4aa60b9a17c44d2194c
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-12 23:13:55 +0530

    Populating Next Right Pointers in Each Node

--- LeetCode_Solutions/Medium/71) Populating Next Right Pointers in Each Node.cpp
+++ LeetCode_Solutions/Medium/71) Populating Next Right Pointers in Each Node.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     Node* connect(Node* root) {
-        
+        if (!root) return root;
+
+        Node* level = root;
+
+        while (level->left) {
+            Node* curr = level;
+
+            while (curr) {
+                curr->left->next = curr->right;
+
+                if (curr->next)
+                    curr->right->next = curr->next->left;
+
+                curr = curr->next;
+            }
+
+            level = level->left;
+        }
+
+        return root;
     }
 };
