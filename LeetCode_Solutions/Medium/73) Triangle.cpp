0db77c172a842810197bd56db15ed52ee300b238
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-14 21:24:19 +0530

    Triangle

--- LeetCode_Solutions/Medium/73) Triangle.cpp
+++ LeetCode_Solutions/Medium/73) Triangle.cpp
@@ -1,1 +1,14 @@
-Given a triangle array, return the minimum path sum from top to bottom.
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        vector<int> dp = triangle.back();
+
+        for (int i = triangle.size() - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1]);
+            }
+        }
+
+        return dp[0];
+    }
+};
