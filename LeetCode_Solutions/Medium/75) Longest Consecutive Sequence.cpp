a5f3d80ea1a55affbe7efaecefe6007627f1a9c1
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-15 22:39:42 +0530

    Longest Consecutive Sequence

--- LeetCode_Solutions/Medium/75) Longest Consecutive Sequence.cpp
+++ LeetCode_Solutions/Medium/75) Longest Consecutive Sequence.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        
+        unordered_set<int> s(nums.begin(), nums.end());
+        int ans = 0;
+
+        for (int x : s) {
+            if (!s.count(x - 1)) {
+                int len = 1;
+                while (s.count(x + len))
+                    len++;
+                ans = max(ans, len);
+            }
+        }
+
+        return ans;
     }
 };
