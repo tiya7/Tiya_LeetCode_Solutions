aa0163aa6bd3c51f9a46a7c133c34f68f2c2f1b3
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-16 22:58:15 +0530

    Sum Root to leaf numbers

--- LeetCode_Solutions/Medium/76) Sum Root to leaf numbers.cpp
+++ LeetCode_Solutions/Medium/76) Sum Root to leaf numbers.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
+    int solve(TreeNode* root, int num) {
+        if (root == NULL) return 0;
+
+        num = num * 10 + root->val;
+
+        if (root->left == NULL && root->right == NULL)
+            return num;
+
+        return solve(root->left, num) + solve(root->right, num);
+    }
+
     int sumNumbers(TreeNode* root) {
-        
+        return solve(root, 0);
     }
 };
