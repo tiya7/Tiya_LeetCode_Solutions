24396da553c932ff2ec2547411c273d6810c685f
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-17 21:36:36 +0530

    Surrounded Regions

--- LeetCode_Solutions/Medium/77) Surrounded Regions.cpp
+++ LeetCode_Solutions/Medium/77) Surrounded Regions.cpp
@@ -4,3 +4,42 @@ public:
         
     }
 };
+class Solution {
+public:
+    void dfs(vector<vector<char>>& board, int i, int j) {
+        int m = board.size(), n = board[0].size();
+
+        if (i < 0 || j < 0 || i >= m || j >= n || board[i][j] != 'O')
+            return;
+
+        board[i][j] = '#';
+
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
+    }
+
+    void solve(vector<vector<char>>& board) {
+        int m = board.size(), n = board[0].size();
+
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, n - 1);
+        }
+
+        for (int j = 0; j < n; j++) {
+            dfs(board, 0, j);
+            dfs(board, m - 1, j);
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O')
+                    board[i][j] = 'X';
+                else if (board[i][j] == '#')
+                    board[i][j] = 'O';
+            }
+        }
+    }
+};
