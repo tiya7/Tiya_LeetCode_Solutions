f058593f8c1c54c5213a4b890c21b2a94a86cbc4
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-18 21:53:23 +0530

    Palindrome Partitioning

--- LeetCode_Solutions/Medium/78) Palindrome Partitioning.cpp
+++ LeetCode_Solutions/Medium/78) Palindrome Partitioning.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
+    vector<vector<string>> ans;
+    vector<string> temp;
+
+    bool isPalindrome(string &s, int l, int r) {
+        while (l < r) {
+            if (s[l] != s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+
+    void solve(string &s, int index) {
+        if (index == s.size()) {
+            ans.push_back(temp);
+            return;
+        }
+
+        for (int i = index; i < s.size(); i++) {
+            if (isPalindrome(s, index, i)) {
+                temp.push_back(s.substr(index, i - index + 1));
+                solve(s, i + 1);
+                temp.pop_back();
+            }
+        }
+    }
+
     vector<vector<string>> partition(string s) {
-        
+        solve(s, 0);
+        return ans;
     }
 };
