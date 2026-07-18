ea7bc05a95594013034608c81d82186448f2c76d
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-18 21:52:11 +0530

    Palindrome Partitioning

---
+++ LeetCode_Solutions/Medium/78) Palindrome Partitioning.cpp
@@ -0,0 +1,89 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+131. Palindrome Partitioning
+Medium
+Topics
+premium lock icon
+Companies
+Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
+
+ 
+
+Example 1:
+
+Input: s = "aab"
+Output: [["a","a","b"],["aa","b"]]
+Example 2:
+
+Input: s = "a"
+Output: [["a"]]
+ 
+
+Constraints:
+
+1 <= s.length <= 16
+s contains only lowercase English letters.
+ 
+Accepted
+1,390,933/1.9M
+Acceptance Rate
+74.3%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+⌄
+⌄
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        
+    }
+};
+Saved
+Testcase
+1
+2
+"aab"
+"a"
+2/8 testcases
+Line 1
+Case 1: s
+Copyright © 2026 LeetCode. All rights reserved.
+
+14.4K
+
+
+264
+Search questions
+
+
