d8c3984c0bdeaa46dd7f3dfba8f324a127ca54da
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-21 21:24:18 +0530

    Single Number II

---
+++ LeetCode_Solutions/Medium/81) Single Number II.cpp
@@ -0,0 +1,92 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+137. Single Number II
+Medium
+Topics
+premium lock icon
+Companies
+Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,3,2]
+Output: 3
+Example 2:
+
+Input: nums = [0,1,0,1,0,1,99]
+Output: 99
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-231 <= nums[i] <= 231 - 1
+Each element in nums appears exactly three times except for one element which appears once.
+ 
+Accepted
+927,299/1.4M
+Acceptance Rate
+67.5%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+⌄
+⌄
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+    }
+};
+Saved
+Testcase
+1
+2
+[2,2,3,2]
+[0,1,0,1,0,1,99]
+2/8 testcases
+Line 1
+Case 1: nums
+Copyright © 2026 LeetCode. All rights reserved.
+
+8.8K
+
+
+212
+Search questions
+
+
