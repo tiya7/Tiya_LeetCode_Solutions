00aa372ecd04b0009afab237120339e403b7f014
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-23 23:01:35 +0530

    Word Break

--- LeetCode_Solutions/Medium/83) Word Break.cpp
+++ LeetCode_Solutions/Medium/83) Word Break.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        
+        unordered_set<string> st(wordDict.begin(), wordDict.end());
+        vector<bool> dp(s.size() + 1, false);
+        dp[0] = true;
+
+        for (int i = 1; i <= s.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && st.count(s.substr(j, i - j))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[s.size()];
     }
 };
