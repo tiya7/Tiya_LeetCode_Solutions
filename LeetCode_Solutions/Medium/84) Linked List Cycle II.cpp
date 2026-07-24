487e18eb7984aec1fbb65e1606de6d8e74b61548
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-24 23:12:12 +0530

    Linked List Cycle II

--- LeetCode_Solutions/Medium/84) Linked List Cycle II.cpp
+++ LeetCode_Solutions/Medium/84) Linked List Cycle II.cpp
@@ -1,6 +1,138 @@
-class Solution {
-public:
-    ListNode *detectCycle(ListNode *head) {
-        
-    }
-};
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+142. Linked List Cycle II
+Medium
+Topics
+premium lock icon
+Companies
+Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed). It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+Do not modify the linked list.
+
+ 
+
+Example 1:
+
+
+Input: head = [3,2,0,-4], pos = 1
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+Example 2:
+
+
+Input: head = [1,2], pos = 0
+Output: tail connects to node index 0
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+Example 3:
+
+
+Input: head = [1], pos = -1
+Output: no cycle
+Explanation: There is no cycle in the linked list.
+ 
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+ 
+
+Follow up: Can you solve it using O(1) (i.e. constant) memory?
+
+ 
+Accepted
+2,124,865/3.6M
+Acceptance Rate
+58.6%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+⌄
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+Saved
+Testcase
+1
+2
+3
+4
+5
+6
+[3,2,0,-4]
+1
+[1,2]
+0
+[1]
+-1
+3/8 testcases
+Line 1
+Case 1: head
+
+Result
+Accepted
+0 ms
+Case 1
+Case 2
+Case 3
+Input
+head =
+[3,2,0,-4]
+pos =
+1
+Output
+tail connects to node index 1
+Expected
+tail connects to node index 1
+Contribute a testcase
+Copyright © 2026 LeetCode. All rights reserved.
+
+15.3K
+
+
+282
+Search questions
+
+
