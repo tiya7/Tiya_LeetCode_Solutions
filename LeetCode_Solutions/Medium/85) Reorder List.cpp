7e3f84f5026337562611fb42e006f1e82e26c0a1
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-25 22:50:19 +0530

    Reorder List

--- LeetCode_Solutions/Medium/85) Reorder List.cpp
+++ LeetCode_Solutions/Medium/85) Reorder List.cpp
@@ -7,3 +7,28 @@ public:
         while (fast->next && fast->next->next) {
             slow = slow->next;
             fast = fast->next->next;
+        }
+
+        ListNode *prev = NULL, *curr = slow->next;
+        slow->next = NULL;
+
+        while (curr) {
+            ListNode *next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        ListNode *first = head, *second = prev;
+        while (second) {
+            ListNode *t1 = first->next;
+            ListNode *t2 = second->next;
+
+            first->next = second;
+            second->next = t1;
+
+            first = t1;
+            second = t2;
+        }
+    }
+};
