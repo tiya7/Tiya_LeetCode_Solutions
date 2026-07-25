5f3a67f880207502ac3b498955ee2d5c5c745b1d
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-25 22:47:23 +0530

    Reorder List

---
+++ LeetCode_Solutions/Medium/85) Reorder List.cpp
@@ -0,0 +1,106 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+143. Reorder List
+Medium
+Topics
+premium lock icon
+Companies
+You are given the head of a singly linked-list. The list can be represented as:
+
+L0 → L1 → … → Ln - 1 → Ln
+Reorder the list to be on the following form:
+
+L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
+You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4]
+Output: [1,4,2,3]
+Example 2:
+
+
+Input: head = [1,2,3,4,5]
+Output: [1,5,2,4,3]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 5 * 104].
+1 <= Node.val <= 1000
+ 
+Accepted
+1,580,799/2.4M
+Acceptance Rate
+65.8%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+⌄
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+Saved
+Testcase
+1
+2
+[1,2,3,4]
+[1,2,3,4,5]
+2/8 testcases
+Line 1
+Case 1: head
+Copyright © 2026 LeetCode. All rights reserved.
+
+12.7K
+
+
+271
+Search questions
+
+
