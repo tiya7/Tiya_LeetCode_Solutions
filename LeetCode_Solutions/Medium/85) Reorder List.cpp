cc626abf4d1dadcd403280e4a6e268867f688299
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-25 22:48:36 +0530

    Reorder List

--- LeetCode_Solutions/Medium/85) Reorder List.cpp
+++ LeetCode_Solutions/Medium/85) Reorder List.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     void reorderList(ListNode* head) {
-        
-    }
-};
+        if (!head || !head->next) return;
+
+        ListNode *slow = head, *fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
