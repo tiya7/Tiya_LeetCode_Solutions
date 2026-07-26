b72b30c5177057b1438086b612caf8b481a7037c
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-26 22:32:34 +0530

    LRU Cache
    Initialize capacity and implement get and put methods for LRU Cache.

--- LeetCode_Solutions/Medium/86) LRU Cache.cpp
+++ LeetCode_Solutions/Medium/86) LRU Cache.cpp
@@ -1,14 +1,35 @@
 class LRUCache {
 public:
+    int cap;
+    list<pair<int,int>> l;
+    unordered_map<int, list<pair<int,int>>::iterator> mp;
+
     LRUCache(int capacity) {
-        
+        cap = capacity;
     }
-    
+
     int get(int key) {
-        
+        if (mp.find(key) == mp.end())
+            return -1;
+
+        auto it = mp[key];
+        int value = it->second;
+        l.erase(it);
+        l.push_front({key, value});
+        mp[key] = l.begin();
+
+        return value;
     }
-    
+
     void put(int key, int value) {
-        
+        if (mp.find(key) != mp.end()) {
+            l.erase(mp[key]);
+        } else if (l.size() == cap) {
+            mp.erase(l.back().first);
+            l.pop_back();
+        }
+
+        l.push_front({key, value});
+        mp[key] = l.begin();
     }
 };
