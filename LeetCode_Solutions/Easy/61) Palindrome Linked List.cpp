66f7ba2d53f008a76559032e6a8c35e5e75ace38
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-07-31 21:48:31 +0530

    Palindrome Linked List

--- LeetCode_Solutions/Easy/61) Palindrome Linked List.cpp
+++ LeetCode_Solutions/Easy/61) Palindrome Linked List.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        
+        vector<int> v;
+
+        while (head) {
+            v.push_back(head->val);
+            head = head->next;
+        }
+
+        int i = 0, j = v.size() - 1;
+
+        while (i < j) {
+            if (v[i] != v[j])
+                return false;
+            i++;
+            j--;
+        }
+
+        return true;
     }
 };
