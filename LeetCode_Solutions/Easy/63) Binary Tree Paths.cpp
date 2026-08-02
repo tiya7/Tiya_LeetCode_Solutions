de8ca08cf89625a3fada94c795fa8f8cf9c30ed3
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-02 22:58:58 +0530

    Binary Tree Paths

--- LeetCode_Solutions/Easy/63) Binary Tree Paths.cpp
+++ LeetCode_Solutions/Easy/63) Binary Tree Paths.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
-        
+        vector<string> ans;
+
+        if (!root)
+            return ans;
+
+        if (!root->left && !root->right) {
+            ans.push_back(to_string(root->val));
+            return ans;
+        }
+
+        vector<string> left = binaryTreePaths(root->left);
+        vector<string> right = binaryTreePaths(root->right);
+
+        for (string s : left)
+            ans.push_back(to_string(root->val) + "->" + s);
+
+        for (string s : right)
+            ans.push_back(to_string(root->val) + "->" + s);
+
+        return ans;
     }
 };
