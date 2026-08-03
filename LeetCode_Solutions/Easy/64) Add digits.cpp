f1a4de5f5800a21df4d9f2918e27300554301cd0
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-03 22:07:58 +0530

    Add digits

---
+++ LeetCode_Solutions/Easy/64) Add digits.cpp
@@ -0,0 +1,96 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+258. Add Digits
+Easy
+Topics
+premium lock icon
+Companies
+Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+ 
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+Example 2:
+
+Input: num = 0
+Output: 0
+ 
+
+Constraints:
+
+0 <= num <= 231 - 1
+ 
+
+Follow up: Could you do it without any loop/recursion in O(1) runtime?
+
+ 
+Accepted
+1,301,160/1.9M
+Acceptance Rate
+69.2%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+⌄
+⌄
+class Solution {
+public:
+    int addDigits(int num) {
+        
+    }
+};
+Saved
+Testcase
+1
+2
+38
+0
+2/8 testcases
+Line 1
+Case 1: num
+Copyright © 2026 LeetCode. All rights reserved.
+
+5.6K
+
+
+146
+Search questions
+
+
