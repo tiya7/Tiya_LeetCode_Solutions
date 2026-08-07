01d7e6ff649e6273fa47aac9522bd3bb956cc099
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-07 23:16:34 +0530

    Word Pattern

--- LeetCode_Solutions/Easy/69) Word Pattern.cpp
+++ LeetCode_Solutions/Easy/69) Word Pattern.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        
+        vector<string> words;
+        string word;
+
+        stringstream ss(s);
+        while (ss >> word)
+            words.push_back(word);
+
+        if (pattern.size() != words.size())
+            return false;
+
+        unordered_map<char, string> mp1;
+        unordered_map<string, char> mp2;
+
+        for (int i = 0; i < pattern.size(); i++) {
+            if (mp1.count(pattern[i]) && mp1[pattern[i]] != words[i])
+                return false;
+
+            if (mp2.count(words[i]) && mp2[words[i]] != pattern[i])
+                return false;
+
+            mp1[pattern[i]] = words[i];
+            mp2[words[i]] = pattern[i];
+        }
+
+        return true;
     }
 };
