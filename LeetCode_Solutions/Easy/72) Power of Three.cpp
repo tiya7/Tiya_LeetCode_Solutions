bbc746dda7a6f2c048a07d35682d2ec01b3e26cd
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-10 20:15:39 +0530

    Power of Three

--- LeetCode_Solutions/Easy/72) Power of Three.cpp
+++ LeetCode_Solutions/Easy/72) Power of Three.cpp
@@ -1,102 +1,12 @@
-
-
-Avatar
-TiyaGandhi
-Access all features with our Premium subscription!
-myLists
-My Lists
-notebook
-Notebook
-progress
-Progress
-points
-Points
-Problems
-Discuss
-Contest
-Interview
-Store
-Try New Features
-Orders
-My Playgrounds
-Settings
-Appearance
-Sign Out
-326. Power of Three
-Easy
-Topics
-premium lock icon
-Companies
-Given an integer n, return true if it is a power of three. Otherwise, return false.
-
-An integer n is a power of three, if there exists an integer x such that n == 3x.
-
- 
-
-Example 1:
-
-Input: n = 27
-Output: true
-Explanation: 27 = 33
-Example 2:
-
-Input: n = 0
-Output: false
-Explanation: There is no x where 3x = 0.
-Example 3:
-
-Input: n = -1
-Output: false
-Explanation: There is no x where 3x = (-1).
- 
-
-Constraints:
-
--231 <= n <= 231 - 1
- 
-
-Follow up: Could you solve it without loops/recursion?
- 
-Accepted
-1,450,263/2.8M
-Acceptance Rate
-51.4%
-C++
-
-
-
-
-1
-2
-3
-4
-5
-6
-⌄
-⌄
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        
+        if (n <= 0)
+            return false;
+
+        while (n % 3 == 0)
+            n = n / 3;
+
+        return n == 1;
     }
 };
-Saved
-Testcase
-1
-2
-3
-27
-0
--1
-3/8 testcases
-Line 1
-Case 1: n
-Copyright © 2026 LeetCode. All rights reserved.
-
-3.7K
-
-
-211
-Search questions
-
-
