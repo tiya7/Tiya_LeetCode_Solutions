882a72d063fed2d4824683de8001f714f24d98cb
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-15 03:06:42 +0530

    Intersection of two arrays

--- LeetCode_Solutions/Easy/77) Intersection of two arrays.cpp
+++ LeetCode_Solutions/Easy/77) Intersection of two arrays.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        
+        set<int> a(nums1.begin(), nums1.end());
+        vector<int> ans;
+
+        for (int x : nums2) {
+            if (a.count(x)) {
+                ans.push_back(x);
+                a.erase(x);
+            }
+        }
+
+        return ans;
     }
 };
