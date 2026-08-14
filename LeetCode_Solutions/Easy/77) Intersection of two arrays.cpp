8c3c21c043938abddf17aa0a21c04f6c59530798
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-15 03:05:21 +0530

    Intersection of two arrays

---
+++ LeetCode_Solutions/Easy/77) Intersection of two arrays.cpp
@@ -0,0 +1,94 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+349. Intersection of Two Arrays
+Easy
+Topics
+premium lock icon
+Companies
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+ 
+Accepted
+1,989,679/2.5M
+Acceptance Rate
+78.2%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+⌄
+⌄
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        
+    }
+};
+Saved
+Testcase
+1
+2
+3
+4
+[1,2,2,1]
+[2,2]
+[4,9,5]
+[9,4,9,8,4]
+2/8 testcases
+Line 1
+Case 1: nums1
+Copyright © 2026 LeetCode. All rights reserved.
+
+7K
+
+
+249
+Search questions
+
+
