d423f4f023d5336ca654488907a0171baf93c5b3
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-16 21:27:47 +0530

    Valid Perfect Square

---
+++ LeetCode_Solutions/Easy/79) Valid Perfect Square.cpp
@@ -0,0 +1,94 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+367. Valid Perfect Square
+Easy
+Topics
+premium lock icon
+Companies
+Given a positive integer num, return true if num is a perfect square or false otherwise.
+
+A perfect square is an integer that is the square of an integer. In other words, it is the product of some integer with itself.
+
+You must not use any built-in library function, such as sqrt.
+
+ 
+
+Example 1:
+
+Input: num = 16
+Output: true
+Explanation: We return true because 4 * 4 = 16 and 4 is an integer.
+Example 2:
+
+Input: num = 14
+Output: false
+Explanation: We return false because 3.742 * 3.742 = 14 and 3.742 is not an integer.
+ 
+
+Constraints:
+
+1 <= num <= 231 - 1
+ 
+Accepted
+981,090/2.2M
+Acceptance Rate
+45.1%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+⌄
+⌄
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        
+    }
+};
+Saved
+Testcase
+1
+2
+16
+14
+2/8 testcases
+Line 1
+Case 1: num
+Copyright © 2026 LeetCode. All rights reserved.
+
+4.7K
+
+
+135
+Search questions
+
+
