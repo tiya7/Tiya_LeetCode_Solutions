e4443e9ca548eb0d80362b0e0b360f756d8da0e1
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-16 21:29:47 +0530

    Valid Perfect Square

--- LeetCode_Solutions/Easy/79) Valid Perfect Square.cpp
+++ LeetCode_Solutions/Easy/79) Valid Perfect Square.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        
+        int i = 1;
+
+        while (i * i <= num) {
+            if (i * i == num)
+                return true;
+            i++;
+        }
+
+        return false;
     }
 };
