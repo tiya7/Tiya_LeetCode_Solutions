b211b84cbeee6558935de47ebc5a74c2fdf1edd7
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-18 22:42:14 +0530

    Ransom note

--- LeetCode_Solutions/Easy/81) Ransom note.cpp
+++ LeetCode_Solutions/Easy/81) Ransom note.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        
+        int count[26] = {};
+
+        for (char c : magazine)
+            count[c - 'a']++;
+
+        for (char c : ransomNote) {
+            if (count[c - 'a'] == 0)
+                return false;
+            count[c - 'a']--;
+        }
+
+        return true;
     }
 };
