9f4bb7d6b1b35424cce2a0e840107692a707ff46
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-19 22:11:53 +0530

    First unique character in a string

--- LeetCode_Solutions/Easy/82) First unique character in a string.cpp
+++ LeetCode_Solutions/Easy/82) First unique character in a string.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        
+        int count[26] = {};
+
+        for (char c : s)
+            count[c - 'a']++;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (count[s[i] - 'a'] == 1)
+                return i;
+        }
+
+        return -1;
     }
 };
