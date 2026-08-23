e320d660929477ae50908d8f841f6f12624c004e
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-23 19:28:10 +0530

    Sum of Left Leaves

--- LeetCode_Solutions/Easy/86) Sum of Left Leaves.cpp
+++ LeetCode_Solutions/Easy/86) Sum of Left Leaves.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int sumOfLeftLeaves(TreeNode* root) {
-        
+        if (!root) return 0;
+
+        int sum = 0;
+
+        if (root->left && !root->left->left && !root->left->right)
+            sum += root->left->val;
+
+        return sum + sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
     }
 };
