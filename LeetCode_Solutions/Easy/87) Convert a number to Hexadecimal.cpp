1ddbdf7b2ba6679f8cf66140a9b904196f1a0ac1
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-24 21:25:39 +0530

    Convert a number to Hexadecimal

--- LeetCode_Solutions/Easy/87) Convert a number to Hexadecimal.cpp
+++ LeetCode_Solutions/Easy/87) Convert a number to Hexadecimal.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     string toHex(int num) {
-        
+        if (num == 0) return "0";
+
+        string ans = "";
+        string hex = "0123456789abcdef";
+
+        while (num != 0) {
+            ans = hex[num & 15] + ans;
+            num >>= 4;
+        }
+
+        return ans;
     }
 };
