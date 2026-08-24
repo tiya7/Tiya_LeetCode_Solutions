852856afab9323d441841c41a974b0dfaa40126e
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-24 21:24:05 +0530

    Convert a number to Hexadecimal

---
+++ LeetCode_Solutions/Easy/87) Convert a number to Hexadecimal.cpp
@@ -0,0 +1,92 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+405. Convert a Number to Hexadecimal
+Easy
+Topics
+premium lock icon
+Companies
+Given a 32-bit integer num, return a string representing its hexadecimal representation. For negative integers, two’s complement method is used.
+
+All the letters in the answer string should be lowercase characters, and there should not be any leading zeros in the answer except for the zero itself.
+
+Note: You are not allowed to use any built-in library method to directly solve this problem.
+
+ 
+
+Example 1:
+
+Input: num = 26
+Output: "1a"
+Example 2:
+
+Input: num = -1
+Output: "ffffffff"
+ 
+
+Constraints:
+
+-231 <= num <= 231 - 1
+ 
+Accepted
+235,464/429.7K
+Acceptance Rate
+54.8%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+⌄
+⌄
+class Solution {
+public:
+    string toHex(int num) {
+        
+    }
+};
+Saved
+Testcase
+1
+2
+26
+-1
+2/8 testcases
+Line 1
+Case 1: num
+Copyright © 2026 LeetCode. All rights reserved.
+
+1.5K
+
+
+35
+Search questions
+
+
