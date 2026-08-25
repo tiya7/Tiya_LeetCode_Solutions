aeb13bd285f47e4e81aa11c00926a27ed73ad3e8
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-25 21:27:48 +0530

    Longest Palindrome

--- LeetCode_Solutions/Easy/88) Longest Palindrome.cpp
+++ LeetCode_Solutions/Easy/88) Longest Palindrome.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        
+        int count[128] = {0};
+
+        for (char c : s) {
+            count[c]++;
+        }
+
+        int ans = 0;
+        bool odd = false;
+
+        for (int i = 0; i < 128; i++) {
+            ans += (count[i] / 2) * 2;
+
+            if (count[i] % 2 == 1)
+                odd = true;
+        }
+
+        if (odd)
+            ans++;
+
+        return ans;
     }
 };
