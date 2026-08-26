771388937403c0e34a7dab6c83891c66eff4308e
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-26 21:43:08 +0530

    Fizz Buzz

---
+++ LeetCode_Solutions/Easy/89) Fizz Buzz.cpp
@@ -0,0 +1,98 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+412. Fizz Buzz
+Easy
+Topics
+premium lock icon
+Companies
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+Example 2:
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+Example 3:
+
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ 
+
+Constraints:
+
+1 <= n <= 104
+ 
+Accepted
+1,877,805/2.5M
+Acceptance Rate
+75.8%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+⌄
+⌄
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        
+    }
+};
+Saved
+Testcase
+1
+2
+3
+3
+5
+15
+3/8 testcases
+Line 1
+Case 1: n
+Copyright © 2026 LeetCode. All rights reserved.
+
+3.5K
+
+
+147
+Search questions
+
+
