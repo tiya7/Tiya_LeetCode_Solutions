f7da0e260ea6af756a183aa18e8d0bb1f82a81d7
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-26 21:46:10 +0530

    Third Maximum Number

---
+++ LeetCode_Solutions/Easy/90) Third Maximum Number.cpp
@@ -0,0 +1,110 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+414. Third Maximum Number
+Easy
+Topics
+premium lock icon
+Companies
+Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,1]
+Output: 1
+Explanation:
+The first distinct maximum is 3.
+The second distinct maximum is 2.
+The third distinct maximum is 1.
+Example 2:
+
+Input: nums = [1,2]
+Output: 2
+Explanation:
+The first distinct maximum is 2.
+The second distinct maximum is 1.
+The third distinct maximum does not exist, so the maximum (2) is returned instead.
+Example 3:
+
+Input: nums = [2,2,3,1]
+Output: 1
+Explanation:
+The first distinct maximum is 3.
+The second distinct maximum is 2 (both 2's are counted together since they have the same value).
+The third distinct maximum is 1.
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Can you find an O(n) solution?
+ 
+Accepted
+913,272/2.3M
+Acceptance Rate
+40.2%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+⌄
+⌄
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        
+    }
+};
+Saved
+Testcase
+1
+2
+3
+[3,2,1]
+[1,2]
+[2,2,3,1]
+3/8 testcases
+Line 1
+Case 1: nums
+Copyright © 2026 LeetCode. All rights reserved.
+
+3.6K
+
+
+168
+Search questions
+
+
