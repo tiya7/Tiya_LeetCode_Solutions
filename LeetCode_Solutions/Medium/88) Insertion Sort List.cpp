593008458a388609007a257f7c66b3f5be80195b
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-26 21:51:00 +0530

    Insertion Sort List

--- LeetCode_Solutions/Medium/88) Insertion Sort List.cpp
+++ LeetCode_Solutions/Medium/88) Insertion Sort List.cpp
@@ -117,4 +117,25 @@ Copyright © 2026 LeetCode. All rights reserved.
 82
 Search questions
 
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode dummy(0);
 
+        while (head) {
+            ListNode* current = head;
+            head = head->next;
+
+            ListNode* temp = &dummy;
+
+            while (temp->next && temp->next->val < current->val) {
+                temp = temp->next;
+            }
+
+            current->next = temp->next;
+            temp->next = current;
+        }
+
+        return dummy.next;
+    }
+};
