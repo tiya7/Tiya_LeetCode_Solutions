8f4c8447a767c1bd8825b3ebb610ad4b8ea6eb63
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-26 21:51:58 +0530

    Insertion Sort List

--- LeetCode_Solutions/Medium/88) Insertion Sort List.cpp
+++ LeetCode_Solutions/Medium/88) Insertion Sort List.cpp
@@ -1,28 +1,6 @@
 class Solution {
 public:
     ListNode* insertionSortList(ListNode* head) {
-        ListNode dummy(0);
-
-        while (head) {
-            ListNode* current = head;
-            head = head->next;
-
-            ListNode* temp = &dummy;
-
-            while (temp->next && temp->next->val < current->val) {
-                temp = temp->next;
-            }
-
-            current->next = temp->next;
-            temp->next = current;
-        }
-
-        return dummy.next;
-    }
-};
-class Solution {
-public:
-    ListNode* insertionSortList(ListNode* head) {
         
     }
 };
