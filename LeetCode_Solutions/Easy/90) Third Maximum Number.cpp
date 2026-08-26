43dd5d81b0f053ee0c9963363aa35d8b215a5586
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-26 21:47:50 +0530

    Third Maximum Number

--- LeetCode_Solutions/Easy/90) Third Maximum Number.cpp
+++ LeetCode_Solutions/Easy/90) Third Maximum Number.cpp
@@ -4,3 +4,24 @@ public:
         
     }
 };
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int count = 1;
+        int last = nums[nums.size() - 1];
+
+        for (int i = nums.size() - 2; i >= 0; i--) {
+            if (nums[i] != last) {
+                count++;
+                last = nums[i];
+
+                if (count == 3)
+                    return last;
+            }
+        }
+
+        return nums[nums.size() - 1];
+    }
+};
