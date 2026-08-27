4cb49acbf0144fe491192dce5dcf20ce543d6329
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-27 21:12:39 +0530

    Number of segments in a string

---
+++ LeetCode_Solutions/Easy/92) Number of segments in a string.cpp
@@ -0,0 +1,70 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+434. Number of Segments in a String
+Easy
+Topics
+premium lock icon
+Companies
+Given a string s, return the number of segments in the string.
+
+A segment is defined to be a contiguous sequence of non-space characters.
+
+ 
+
+Example 1:
+
+Input: s = "Hello, my name is John"
+Output: 5
+Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+Example 2:
+
+Input: s = "Hello"
+Output: 1
+ 
+
+Constraints:
+
+0 <= s.length <= 300
+s consists of lowercase and uppercase English letters, digits, or one of the following characters "!@#$%^&*()_+-=',.:".
+The only space character in s is ' '.
+ 
+Accepted
+298,688/792.3K
+Acceptance Rate
+37.7%
+Testcase
+1
+2
+"Hello, my name is John"
+"Hello"
+2/8 testcases
+Line 1
+Case 1: s
+Copyright © 2026 LeetCode. All rights reserved.
+
+932
+
+
+53
