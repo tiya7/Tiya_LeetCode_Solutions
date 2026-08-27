824871a6303fb0703dc21c61ed8aac3d4deeb8a5
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-27 21:02:38 +0530

    Add Strings
    Implement the addStrings function to sum two string-represented integers without converting them to integers directly.

---
+++ LeetCode_Solutions/Easy/91) Add Strings.cpp
@@ -0,0 +1,119 @@
+LeetCode Logo
+Debugging...
+Debugging...
+
+
+
+
+
+
+
+
+
+0
+
+avatar
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+Description
+Editorial
+Editorial
+Solutions
+Solutions
+Submissions
+Submissions
+
+
+Code
+Testcase
+Testcase
+Test Result
+415. Add Strings
+Easy
+Topics
+premium lock icon
+Companies
+Given two non-negative integers, num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+
+You must solve the problem without using any built-in library for handling large integers (such as BigInteger). You must also not convert the inputs to integers directly.
+
+ 
+
+Example 1:
+
+Input: num1 = "11", num2 = "123"
+Output: "134"
+Example 2:
+
+Input: num1 = "456", num2 = "77"
+Output: "533"
+Example 3:
+
+Input: num1 = "0", num2 = "0"
+Output: "0"
+ 
+
+Constraints:
+
+1 <= num1.length, num2.length <= 104
+num1 and num2 consist of only digits.
+num1 and num2 don't have any leading zeros except for the zero itself.
+ 
+Seen this question in a real interview before?
+1/6
+Yes
+No
+Accepted
+981,271/1.9M
+Acceptance Rate
+52.2%
+Topics
+icon
+Companies
+Similar Questions
+Discussion (107)
+
+
+Choose a type
+
+
+
+Copyright © 2026 LeetCode. All rights reserved.
+
+5.5K
+
+
+107
+
+21 Online
+C++
+Auto
+
+
+
+
+
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        
+    }
+};
+123456
+Saved
+You must run your code first
