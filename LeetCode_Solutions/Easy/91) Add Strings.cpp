de34b597d07c02b24302cd28127a5737ec69d79a
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-27 21:03:54 +0530

    Add implementation for string addition
    Implement the addStrings function to add two numbers represented as strings.

--- LeetCode_Solutions/Easy/91) Add Strings.cpp
+++ LeetCode_Solutions/Easy/91) Add Strings.cpp
@@ -4,3 +4,28 @@ public:
         
     }
 };
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        string ans = "";
+        int i = num1.size() - 1;
+        int j = num2.size() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+
+            if (i >= 0)
+                sum += num1[i--] - '0';
+
+            if (j >= 0)
+                sum += num2[j--] - '0';
+
+            ans += char(sum % 10 + '0');
+            carry = sum / 10;
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
