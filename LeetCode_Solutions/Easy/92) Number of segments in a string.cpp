d76b426a7dab1a2bf80fac9919c4db9e4c736f25
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-27 21:18:09 +0530

    Number of segments in a string

--- LeetCode_Solutions/Easy/92) Number of segments in a string.cpp
+++ LeetCode_Solutions/Easy/92) Number of segments in a string.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int countSegments(string s) {
-        
-    }
-};
-class Solution {
-public:
-    int countSegments(string s) {
         int count = 0;
 
         for (int i = 0; i < s.length(); i++) {
