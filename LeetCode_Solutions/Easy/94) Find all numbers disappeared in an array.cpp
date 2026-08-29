e40f9db7b014f68406410de167c544f0a272735d
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-29 21:58:57 +0530

    Find all numbers disappeared in an array

---
+++ LeetCode_Solutions/Easy/94) Find all numbers disappeared in an array.cpp
@@ -0,0 +1,94 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+448. Find All Numbers Disappeared in an Array
+Easy
+Topics
+premium lock icon
+Companies
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 105
+1 <= nums[i] <= n
+ 
+
+Follow up: Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+ 
+Accepted
+1,497,673/2.3M
+Acceptance Rate
+64.6%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+⌄
+⌄
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        
+    }
+};
+Saved
+Testcase
+1
+2
+[4,3,2,7,8,2,3,1]
+[1,1]
+2/8 testcases
+Line 1
+Case 1: nums
+Copyright © 2026 LeetCode. All rights reserved.
+
+10.6K
+
+
+165
+Search questions
+
+
