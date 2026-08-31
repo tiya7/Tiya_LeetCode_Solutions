d6878c1607000bf711dfbdff6e4de25919cef3e2
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-01 00:20:15 +0530

    Sort List

--- LeetCode_Solutions/Medium/89) Sort List.cpp
+++ LeetCode_Solutions/Medium/89) Sort List.cpp
@@ -3,4 +3,113 @@ public:
     ListNode* sortList(ListNode* head) {
         
     }
-};
+}
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+148. Sort List
+Medium
+Topics
+premium lock icon
+Companies
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+ 
+
+Example 1:
+
+
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+Example 2:
+
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 5 * 104].
+-105 <= Node.val <= 105
+ 
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+
+ 
+Accepted
+1,372,177/2.1M
+Acceptance Rate
+65.3%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+⌄
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+Saved
+Testcase
+1
+2
+3
+[4,2,1,3]
+[-1,5,3,4,0]
+[]
+3/8 testcases
+Line 1
+Case 1: head
+Copyright © 2026 LeetCode. All rights reserved.
+
+13.2K
+
+
+203
+Search questions
+
+
