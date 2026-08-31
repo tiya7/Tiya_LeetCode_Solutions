911546c07b3b318a5ab962bd41a0288e3c4546e0
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-08-31 19:39:23 +0530

    Repeated Substring Pattern

--- LeetCode_Solutions/Easy/96) Repeated Substring Pattern.cpp
+++ LeetCode_Solutions/Easy/96) Repeated Substring Pattern.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        
-    }
-};
-class Solution {
-public:
-    bool repeatedSubstringPattern(string s) {
         int n = s.size();
 
         for (int len = 1; len <= n / 2; len++) {
             if (n % len == 0) {
                 string part = s.substr(0, len);
                 string formed = "";
-
 
                 for (int i = 0; i < n / len; i++)
                     formed += part;
