48af7e684b77eead2276b50bd8ec0f6cbcb8d9cd
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-01 21:36:34 +0530

    Update 97) Hamming Distance.cpp

--- LeetCode_Solutions/Easy/97) Hamming Distance.cpp
+++ LeetCode_Solutions/Easy/97) Hamming Distance.cpp
@@ -4,3 +4,17 @@ public:
         
     }
 };
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int n = x ^ y;
+        int count = 0;
+
+        while (n > 0) {
+            count += n % 2;
+            n = n / 2;
+        }
+
+        return count;
+    }
+};
