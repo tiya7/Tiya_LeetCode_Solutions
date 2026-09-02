97a69dc073d0e10387f409b475c884dc381bc628
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-03 00:09:41 +0530

    Add Reverse Words in a String problem statement
    This file contains the problem statement and a class definition for reversing words in a string.

---
+++ LeetCode_Solutions/Medium/91) Reverse Words in a String.cpp
@@ -0,0 +1,134 @@
+LeetCode Logo
+Problem List
+Debugging...
+Debugging...
+
+
+
+
+
+
+
+
+
+0
+
+avatar
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+Premium
+Description
+Editorial
+Editorial
+Solutions
+Solutions
+Submissions
+Submissions
+
+
+Code
+Testcase
+Testcase
+Test Result
+151. Reverse Words in a String
+Medium
+Topics
+premium lock icon
+Companies
+Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+ 
+
+Example 1:
+
+Input: s = "the sky is blue"
+Output: "blue is sky the"
+Example 2:
+
+Input: s = "  hello world  "
+Output: "world hello"
+Explanation: Your reversed string should not contain leading or trailing spaces.
+Example 3:
+
+Input: s = "a good   example"
+Output: "example good a"
+Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+There is at least one word in s.
+ 
+
+Follow-up: If the string data type is mutable in your language, can you solve it in-place with O(1) extra space?
+
+ 
+Seen this question in a real interview before?
+1/6
+Yes
+No
+Accepted
+3,238,791/5.6M
+Acceptance Rate
+57.7%
+Topics
+icon
+Companies
+Similar Questions
+Discussion (382)
+
+
+Choose a type
+
+
+
+Copyright © 2026 LeetCode. All rights reserved.
+
+10.9K
+
+
+382
+
+192 Online
+C++
+Auto
+
+
+
+
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        
+    }
+};
+123456
+Saved
+You must run your code first
+Search questions
+
+
