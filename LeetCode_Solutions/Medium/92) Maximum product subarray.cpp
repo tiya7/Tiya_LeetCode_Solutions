6106bdae000f12ece46726e24bee697db2336bcd
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-04 00:24:47 +0530

    Maximum product subarray

--- LeetCode_Solutions/Medium/92) Maximum product subarray.cpp
+++ LeetCode_Solutions/Medium/92) Maximum product subarray.cpp
@@ -18,3 +18,20 @@ public:
         return answer;
     }
 };
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int ans = nums[0];
+
+        for (int i = 0; i < nums.size(); i++) {
+            int product = 1;
+
+            for (int j = i; j < nums.size(); j++) {
+                product = product * nums[j];
+                ans = max(ans, product);
+            }
+        }
+
+        return ans;
+    }
+};
