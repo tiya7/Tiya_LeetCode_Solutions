4bec6b08068cbf1a87aac13e8f766b52142327b2
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-04 00:21:40 +0530

    Maximum product subarray

---
+++ LeetCode_Solutions/Medium/92) Maximum product subarray.cpp
@@ -0,0 +1,96 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+152. Maximum Product Subarray
+Medium
+Topics
+premium lock icon
+Companies
+Given an integer array nums, find a subarray that has the largest product, and return the product.
+
+The test cases are generated so that the answer will fit in a 32-bit integer.
+
+Note that the product of an array with a single element is the value of that element.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,-2,4]
+Output: 6
+Explanation: [2,3] has the largest product 6.
+Example 2:
+
+Input: nums = [-2,0,-1]
+Output: 0
+Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+-10 <= nums[i] <= 10
+The product of any subarray of nums is guaranteed to fit in a 32-bit integer.
+ 
+Accepted
+2,195,310/5.9M
+Acceptance Rate
+37.0%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+⌄
+⌄
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+    }
+};
+Saved
+Testcase
+1
+2
+[2,3,-2,4]
+[-2,0,-1]
+2/8 testcases
+Line 1
+Case 1: nums
+Copyright © 2026 LeetCode. All rights reserved.
+
+20.8K
+
+
+474
+Search questions
+
+
