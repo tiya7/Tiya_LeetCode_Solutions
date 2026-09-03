a0ed628f998f00d75fd5b6010c58029484a15483
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-04 00:25:16 +0530

    Maximum product subarray

--- LeetCode_Solutions/Medium/92) Maximum product subarray.cpp
+++ LeetCode_Solutions/Medium/92) Maximum product subarray.cpp
@@ -1,26 +1,6 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int maximum = nums[0];
-        int minimum = nums[0];
-        int answer = nums[0];
-
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] < 0)
-                swap(maximum, minimum);
-
-            maximum = max(nums[i], maximum * nums[i]);
-            minimum = min(nums[i], minimum * nums[i]);
-
-            answer = max(answer, maximum);
-        }
-
-        return answer;
-    }
-};
-class Solution {
-public:
-    int maxProduct(vector<int>& nums) {
         int ans = nums[0];
 
         for (int i = 0; i < nums.size(); i++) {
