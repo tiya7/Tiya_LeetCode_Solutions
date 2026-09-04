d162a3319fae509217ea64ca7be35b6c26d94077
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-05 01:21:21 +0530

    Number Complement

--- LeetCode_Solutions/Easy/99) Number Complement.cpp
+++ LeetCode_Solutions/Easy/99) Number Complement.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int findComplement(int num) {
-        
+        int mask = 0;
+        int temp = num;
+
+        while (temp > 0) {
+            mask = mask * 2 + 1;
+            temp = temp / 2;
+        }
+
+        return num ^ mask;
     }
 };
