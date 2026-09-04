7b446da8e1057a2ca26fae11b1a2366555e48889
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-04 21:40:43 +0530

    Create 98) Island Perimeter.cpp

---
+++ LeetCode_Solutions/Easy/98) Island Perimeter.cpp
@@ -0,0 +1,130 @@
+LeetCode Logo
+Problem List
+Debugging...
+Debugging...
+
+
+
+
+
+
+
+
+
+0
+
+avatar
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+Premium
+Description
+Editorial
+Editorial
+Solutions
+Solutions
+Submissions
+Submissions
+
+
+Code
+Testcase
+Testcase
+Test Result
+463. Island Perimeter
+Easy
+Topics
+premium lock icon
+Companies
+You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+Output: 16
+Explanation: The perimeter is the 16 yellow stripes in the image above.
+Example 2:
+
+Input: grid = [[1]]
+Output: 4
+Example 3:
+
+Input: grid = [[1,0]]
+Output: 4
+ 
+
+Constraints:
+
+row == grid.length
+col == grid[i].length
+1 <= row, col <= 100
+grid[i][j] is 0 or 1.
+There is exactly one island in grid.
+ 
+Seen this question in a real interview before?
+1/6
+Yes
+No
+Accepted
+867,458/1.2M
+Acceptance Rate
+74.4%
+Topics
+icon
+Companies
+Similar Questions
+Discussion (213)
+
+
+Choose a type
+
+
+
+Copyright © 2026 LeetCode. All rights reserved.
+
+7.4K
+
+
+213
+
+30 Online
+C++
+Auto
+
+
+
+
+
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        
+    }
+};
+123456
+Saved
+You must run your code first
+Search questions
+
+
