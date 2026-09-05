7fd382f07aa869e5bc23b1c318de9a9db73495bb
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-05 19:36:58 +0530

    Find Minimum in Rotated Sorted Array

---
+++ LeetCode_Solutions/Medium/93) Find Minimum in Rotated Sorted Array.cpp
@@ -0,0 +1,109 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+153. Find Minimum in Rotated Sorted Array
+Medium
+Topics
+premium lock icon
+Companies
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+[4,5,6,7,0,1,2] if it was rotated 4 times.
+[0,1,2,4,5,6,7] if it was rotated 7 times.
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+You must write an algorithm that runs in O(log n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: 1
+Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2]
+Output: 0
+Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+Example 3:
+
+Input: nums = [11,13,15,17]
+Output: 11
+Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5000
+-5000 <= nums[i] <= 5000
+All the integers of nums are unique.
+nums is sorted and rotated between 1 and n times.
+ 
+Accepted
+3,361,421/6.1M
+Acceptance Rate
+55.3%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+⌄
+⌄
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        
+    }
+};
+Saved
+Testcase
+1
+2
+3
+[3,4,5,1,2]
+[4,5,6,7,0,1,2]
+[11,13,15,17]
+3/8 testcases
+Line 1
+Case 1: nums
+Copyright © 2026 LeetCode. All rights reserved.
+
+15.8K
+
+
+505
+Search questions
+
+
