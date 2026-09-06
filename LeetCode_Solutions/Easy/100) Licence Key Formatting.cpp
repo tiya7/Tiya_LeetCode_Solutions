29369d8d37bd5bb96f59706722ae5c893907837c
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-06 18:05:34 +0530

    Licence Key Formatting

---
+++ LeetCode_Solutions/Easy/100) Licence Key Formatting.cpp
@@ -0,0 +1,101 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+482. License Key Formatting
+Easy
+Topics
+premium lock icon
+Companies
+You are given a license key represented as a string s that consists of only alphanumeric characters and dashes. The string is separated into n + 1 groups by n dashes. You are also given an integer k.
+
+We want to reformat the string s such that each group contains exactly k characters, except for the first group, which could be shorter than k but still must contain at least one character. Furthermore, there must be a dash inserted between two groups, and you should convert all lowercase letters to uppercase.
+
+Return the reformatted license key.
+
+ 
+
+Example 1:
+
+Input: s = "5F3Z-2e-9-w", k = 4
+Output: "5F3Z-2E9W"
+Explanation: The string s has been split into two parts, each part has 4 characters.
+Note that the two extra dashes are not needed and can be removed.
+Example 2:
+
+Input: s = "2-5g-3-J", k = 2
+Output: "2-5G-3J"
+Explanation: The string s has been split into three parts, each part has 2 characters except the first part as it could be shorter as mentioned above.
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of English letters, digits, and dashes '-'.
+1 <= k <= 104
+ 
+Accepted
+378,585/814.2K
+Acceptance Rate
+46.5%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+⌄
+⌄
+class Solution {
+public:
+    string licenseKeyFormatting(string s, int k) {
+        
+    }
+};
+Saved
+Testcase
+1
+2
+3
+4
+"5F3Z-2e-9-w"
+4
+"2-5g-3-J"
+2
+2/8 testcases
+Line 1
+Case 1: s
+Copyright © 2026 LeetCode. All rights reserved.
+
+1.2K
+
+
+66
+Search questions
+
+
