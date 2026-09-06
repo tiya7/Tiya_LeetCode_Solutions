73cf2c186f26f53568c8cb8abe0da115ac17d4a5
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-06 18:07:34 +0530

    Update 100) Licence Key Formatting

--- LeetCode_Solutions/Easy/100) Licence Key Formatting.cpp
+++ LeetCode_Solutions/Easy/100) Licence Key Formatting.cpp
@@ -4,3 +4,32 @@ public:
         
     }
 };
+class Solution {
+public:
+    string licenseKeyFormatting(string s, int k) {
+        string temp = "";
+
+        for (char c : s) {
+            if (c != '-') {
+                temp += toupper(c);
+            }
+        }
+
+        string ans = "";
+        int count = 0;
+
+        for (int i = temp.size() - 1; i >= 0; i--) {
+            if (count == k) {
+                ans += '-';
+                count = 0;
+            }
+
+            ans += temp[i];
+            count++;
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
