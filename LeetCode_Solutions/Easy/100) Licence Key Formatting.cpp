e0e48756d585f716605fd7ba7b640fdcd0d51cab
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-06 18:07:48 +0530

    Licence Key Formatting

--- LeetCode_Solutions/Easy/100) Licence Key Formatting.cpp
+++ LeetCode_Solutions/Easy/100) Licence Key Formatting.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     string licenseKeyFormatting(string s, int k) {
-        
-    }
-};
-class Solution {
-public:
-    string licenseKeyFormatting(string s, int k) {
         string temp = "";
 
         for (char c : s) {
