46a6ea323ffdb8fd3d60ba0d1202dfd48f495f54
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-07 22:58:42 +0530

    Update 94) Min Stack.cpp

--- LeetCode_Solutions/Medium/94) Min Stack.cpp
+++ LeetCode_Solutions/Medium/94) Min Stack.cpp
@@ -1,123 +1,4 @@
-LeetCode Logo
-Problem List
-Debugging...
-Debugging...
 
-
-
-
-
-
-
-
-
--
-
-avatar
-Avatar
-TiyaGandhi
-Access all features with our Premium subscription!
-myLists
-My Lists
-notebook
-Notebook
-progress
-Progress
-points
-Points
-Try New Features
-Orders
-My Playgrounds
-Settings
-Appearance
-Sign Out
-Premium
-Description
-Editorial
-Editorial
-Solutions
-Solutions
-Submissions
-Submissions
-Code
-Testcase
-Testcase
-Test Result
-155. Min Stack
-Medium
-Topics
-premium lock icon
-Companies
-Hint
-Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
-
-Implement the MinStack class:
-
-MinStack() initializes the stack object.
-void push(int value) pushes the element value onto the stack.
-void pop() removes the element on the top of the stack.
-int top() gets the top element of the stack.
-int getMin() retrieves the minimum element in the stack.
-You must implement a solution with O(1) time complexity for each function.
-
- 
-
-Example 1:
-
-Input
-["MinStack","push","push","push","getMin","pop","top","getMin"]
-[[],[-2],[0],[-3],[],[],[],[]]
-
-Output
-[null,null,null,null,-3,null,0,-2]
-
-Explanation
-MinStack minStack = new MinStack();
-minStack.push(-2);
-minStack.push(0);
-minStack.push(-3);
-minStack.getMin(); // return -3
-minStack.pop();
-minStack.top();    // return 0
-minStack.getMin(); // return -2
- 
-
-Constraints:
-
--231 <= val <= 231 - 1
-Methods pop, top and getMin operations will always be called on non-empty stacks.
-At most 3 * 104 calls will be made to push, pop, top, and getMin.
- 
-Seen this question in a real interview before?
-1/6
-Yes
-No
-Accepted
-2,939,224/5M
-Acceptance Rate
-58.6%
-Topics
-icon
-Companies
-Hint 1
-Similar Questions
-Discussion (347)
-
-
-Choose a type
-
-
-
-Copyright © 2026 LeetCode. All rights reserved.
-
-16.3K
-
-
-347
-
-232 Online
-You must run your code first
-Search questions
 class MinStack {
 public:
     MinStack() {
@@ -141,3 +22,34 @@ public:
     }
 };
 
+class MinStack {
+public:
+    stack<int> s;
+    stack<int> minimum;
+
+    MinStack() {
+        
+    }
+    
+    void push(int value) {
+        s.push(value);
+
+        if (minimum.empty() || value <= minimum.top())
+            minimum.push(value);
+    }
+    
+    void pop() {
+        if (s.top() == minimum.top())
+            minimum.pop();
+
+        s.pop();
+    }
+    
+    int top() {
+        return s.top();
+    }
+    
+    int getMin() {
+        return minimum.top();
+    }
+};
