26d65af234a393349f102b071bf24cb9cd97049c
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-08 22:29:28 +0530

    Find Peak Element

---
+++ LeetCode_Solutions/Medium/95) Find Peak Element.cpp
@@ -0,0 +1,98 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+162. Find Peak Element
+Medium
+Topics
+premium lock icon
+Companies
+A peak element is an element that is strictly greater than its neighbors.
+
+Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+
+You must write an algorithm that runs in O(log n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 2
+Explanation: 3 is a peak element and your function should return the index number 2.
+Example 2:
+
+Input: nums = [1,2,1,3,5,6,4]
+Output: 5
+Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+-231 <= nums[i] <= 231 - 1
+nums[i] != nums[i + 1] for all valid i.
+ 
+Accepted
+2,611,237/5.5M
+Acceptance Rate
+47.2%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+⌄
+⌄
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        
+    }
+};
+Saved
+Testcase
+1
+2
+[1,2,3,1]
+[1,2,1,3,5,6,4]
+2/8 testcases
+Line 1
+Case 1: nums
+Copyright © 2026 LeetCode. All rights reserved.
+
+15K
+
+
+574
+Search questions
+
+
