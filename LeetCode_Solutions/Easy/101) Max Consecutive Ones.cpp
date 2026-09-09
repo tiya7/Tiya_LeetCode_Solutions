82bc39398e23191e52118a978d0df3cbd63020b3
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-09 22:38:32 +0530

    Max Consecutive Ones

--- LeetCode_Solutions/Easy/101) Max Consecutive Ones.cpp
+++ LeetCode_Solutions/Easy/101) Max Consecutive Ones.cpp
@@ -4,3 +4,21 @@ public:
         
     }
 };
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count = 0;
+        int ans = 0;
+
+        for (int x : nums) {
+            if (x == 1) {
+                count++;
+                ans = max(ans, count);
+            } else {
+                count = 0;
+            }
+        }
+
+        return ans;
+    }
+};
