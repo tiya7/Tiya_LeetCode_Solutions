7d0913e6c7e3e86dc0c0778067144d73b2b29650
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-10 00:29:03 +0530

    Maximum Gap

--- LeetCode_Solutions/Medium/96) Maximum Gap.cpp
+++ LeetCode_Solutions/Medium/96) Maximum Gap.cpp
@@ -1,99 +1,17 @@
-
-
-Avatar
-TiyaGandhi
-Access all features with our Premium subscription!
-myLists
-My Lists
-notebook
-Notebook
-progress
-Progress
-points
-Points
-Problems
-Discuss
-Contest
-Interview
-Store
-Try New Features
-Orders
-My Playgrounds
-Settings
-Appearance
-Sign Out
-164. Maximum Gap
-Medium
-Topics
-premium lock icon
-Companies
-Given an integer array nums, return the maximum difference between two successive elements in its sorted form. If the array contains less than two elements, return 0.
-
-You must write an algorithm that runs in linear time and uses linear extra space.
-
- 
-
-Example 1:
-
-Input: nums = [3,6,9,1]
-Output: 3
-Explanation: The sorted form of the array is [1,3,6,9], either (3,6) or (6,9) has the maximum difference 3.
-Example 2:
-
-Input: nums = [10]
-Output: 0
-Explanation: The array contains less than 2 elements, therefore return 0.
- 
-
-Constraints:
-
-1 <= nums.length <= 105
-0 <= nums[i] <= 109
- 
-Accepted
-372,804/702.9K
-Acceptance Rate
-53.0%
-C++
-
-
-
-
-1
-2
-3
-4
-5
-6
-⌄
-⌄
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        
+        if (nums.size() < 2)
+            return 0;
+
+        sort(nums.begin(), nums.end());
+
+        int ans = 0;
+
+        for (int i = 1; i < nums.size(); i++) {
+            ans = max(ans, nums[i] - nums[i - 1]);
+        }
+
+        return ans;
     }
 };
-Saved
-Testcase
-1
-2
-[3,6,9,1]
-[10]
-2/8 testcases
-Line 1
-Case 1: nums
-Copyright © 2026 LeetCode. All rights reserved.
-
-3.6K
-
-
-117
-Search questions
-
-
-class Solution {
-public:
-    int maximumGap(vector<int>& nums) {
-        
-;    }
-}
