6086919215de51fdb9a084766f3b7b56162b40fa
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-10 18:20:53 +0530

    Update 97) Compare Version Numbers

--- LeetCode_Solutions/Medium/97) Compare Version Numbers.cpp
+++ LeetCode_Solutions/Medium/97) Compare Version Numbers.cpp
@@ -4,3 +4,28 @@ public:
         
     }
 };
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        stringstream a(version1);
+        stringstream b(version2);
+
+        string x, y;
+
+        while (getline(a, x, '.') || getline(b, y, '.')) {
+            int n1 = x.empty() ? 0 : stoi(x);
+            int n2 = y.empty() ? 0 : stoi(y);
+
+            if (n1 < n2)
+                return -1;
+
+            if (n1 > n2)
+                return 1;
+
+            x = "";
+            y = "";
+        }
+
+        return 0;
+    }
+};
