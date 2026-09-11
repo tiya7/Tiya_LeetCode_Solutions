6ce7459b4de17e765ddd321b4bd0d92700139013
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-11 23:22:06 +0530

    Two Sum II - Input Array is Sorted

---
+++ LeetCode_Solutions/Medium/99) Two Sum II - Input Array is Sorted.cpp
@@ -0,0 +1,113 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+167. Two Sum II - Input Array Is Sorted
+Medium
+Topics
+premium lock icon
+Companies
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers index1 and index2, each incremented by one, as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+ 
+
+Constraints:
+
+2 <= numbers.length <= 3 * 104
+-1000 <= numbers[i] <= 1000
+numbers is sorted in non-decreasing order.
+-1000 <= target <= 1000
+The tests are generated such that there is exactly one solution.
+ 
+Accepted
+3,746,609/5.7M
+Acceptance Rate
+65.9%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+⌄
+⌄
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        
+    }
+};
+Saved
+Testcase
+1
+2
+3
+4
+5
+6
+[2,7,11,15]
+9
+[2,3,4]
+6
+[-1,0]
+-1
+3/8 testcases
+Line 1
+Case 1: numbers
+Copyright © 2026 LeetCode. All rights reserved.
+
+13.8K
+
+
+488
+Search questions
+
+
