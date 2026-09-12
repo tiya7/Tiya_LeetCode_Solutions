d04b713f64c940240a64626a6dc0f076b8ade6f0
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-12 22:57:54 +0530

    Factorial Trailing Zeroes

---
+++ LeetCode_Solutions/Medium/100) Factorial Trailing Zeroes.cpp
@@ -0,0 +1,82 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+172. Factorial Trailing Zeroes
+Medium
+Topics
+premium lock icon
+Companies
+Given an integer n, return the number of trailing zeroes in n!.
+
+Note that n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: 0
+Explanation: 3! = 6, no trailing zero.
+Example 2:
+
+Input: n = 5
+Output: 1
+Explanation: 5! = 120, one trailing zero.
+Example 3:
+
+Input: n = 0
+Output: 0
+ 
+
+Constraints:
+
+0 <= n <= 104
+ 
+
+Follow up: Could you write a solution that works in logarithmic time complexity?
+
+ 
+Accepted
+714,689/1.5M
+Acceptance Rate
+47.4%
+Testcase
+1
+2
+3
+3
+5
+0
+3/8 testcases
+Line 1
+Case 1: n
+Copyright © 2026 LeetCode. All rights reserved.
+
+3.6K
+
+
+131
+Search questions
+
+
