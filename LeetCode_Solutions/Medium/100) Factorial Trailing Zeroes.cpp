ee4d90fb24515a3001a19613058c4bf5a9d404ec
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-12 23:00:47 +0530

    Factorial Trailing Zeroes

--- LeetCode_Solutions/Medium/100) Factorial Trailing Zeroes.cpp
+++ LeetCode_Solutions/Medium/100) Factorial Trailing Zeroes.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        
-    }
-};
-class Solution {
-public:
-    int trailingZeroes(int n) {
         int count = 0;
 
         while (n > 0) {
