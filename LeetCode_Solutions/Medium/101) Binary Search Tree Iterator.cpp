2f3002cf2929cd111294546ddcc63059f2570bc3
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-13 07:38:45 +0530

    Binary Search Tree Iterator

--- LeetCode_Solutions/Medium/101) Binary Search Tree Iterator.cpp
+++ LeetCode_Solutions/Medium/101) Binary Search Tree Iterator.cpp
@@ -1,14 +1,29 @@
 class BSTIterator {
 public:
+    stack<TreeNode*> st;
+
     BSTIterator(TreeNode* root) {
-        
+        pushLeft(root);
     }
-    
+
+    void pushLeft(TreeNode* root) {
+        while (root != NULL) {
+            st.push(root);
+            root = root->left;
+        }
+    }
+
     int next() {
-        
+        TreeNode* node = st.top();
+        st.pop();
+
+        if (node->right != NULL)
+            pushLeft(node->right);
+
+        return node->val;
     }
-    
+
     bool hasNext() {
-        
+        return !st.empty();
     }
 };
