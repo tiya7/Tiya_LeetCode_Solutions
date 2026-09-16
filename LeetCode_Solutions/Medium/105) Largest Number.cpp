5f062b269654b12e55ff8e1efed65de95ed2b103
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-17 00:30:28 +0530

    Largest Number

--- LeetCode_Solutions/Medium/105) Largest Number.cpp
+++ LeetCode_Solutions/Medium/105) Largest Number.cpp
@@ -4,3 +4,26 @@ public:
         
     }
 };
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> v;
+
+        for (int x : nums)
+            v.push_back(to_string(x));
+
+        sort(v.begin(), v.end(), [](string a, string b) {
+            return a + b > b + a;
+        });
+
+        if (v[0] == "0")
+            return "0";
+
+        string ans = "";
+
+        for (string x : v)
+            ans += x;
+
+        return ans;
+    }
+};
