6d711e52967b75b0b4a3c56e36566370b5b4dbaf
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-19 00:21:35 +0530

    Department Highest Salary

--- LeetCode_Solutions/Medium/107) Department Highest Salary.cpp
+++ LeetCode_Solutions/Medium/107) Department Highest Salary.cpp
@@ -130,3 +130,16 @@ Copyright © 2026 LeetCode. All rights reserved.
 
 155
 Search questions
+
+SELECT 
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM Employee e
+JOIN Department d
+    ON e.departmentId = d.id
+WHERE e.salary = (
+    SELECT MAX(e2.salary)
+    FROM Employee e2
+    WHERE e2.departmentId = e.departmentId
+);
