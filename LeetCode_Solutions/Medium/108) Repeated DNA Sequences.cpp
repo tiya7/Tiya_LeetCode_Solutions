fb6fc49bdee3c4e4e809e3f233ec7038d9582cf9
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-19 15:44:38 +0530

    Update 108) Repeated DNA Sequences

--- LeetCode_Solutions/Medium/108) Repeated DNA Sequences.cpp
+++ LeetCode_Solutions/Medium/108) Repeated DNA Sequences.cpp
@@ -4,3 +4,22 @@ public:
         
     }
 };
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        map<string, int> mp;
+        vector<string> ans;
+
+        for (int i = 0; i + 10 <= s.size(); i++) {
+            string x = s.substr(i, 10);
+            mp[x]++;
+        }
+
+        for (auto x : mp) {
+            if (x.second > 1)
+                ans.push_back(x.first);
+        }
+
+        return ans;
+    }
+};
