cb1fe5968306b249c2305e0964c54db55cb535e2
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-19 15:43:05 +0530

    Repeated DNA Sequences

---
+++ LeetCode_Solutions/Medium/108) Repeated DNA Sequences.cpp
@@ -0,0 +1,92 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+187. Repeated DNA Sequences
+Medium
+Topics
+premium lock icon
+Companies
+The DNA sequence is composed of a series of nucleotides abbreviated as 'A', 'C', 'G', and 'T'.
+
+For example, "ACGAATTCCG" is a DNA sequence.
+When studying DNA, it is useful to identify repeated sequences within the DNA.
+
+Given a string s that represents a DNA sequence, return all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+Output: ["AAAAACCCCC","CCCCCAAAAA"]
+Example 2:
+
+Input: s = "AAAAAAAAAAAAA"
+Output: ["AAAAAAAAAA"]
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is either 'A', 'C', 'G', or 'T'.
+ 
+Accepted
+566,247/1M
+Acceptance Rate
+54.0%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+⌄
+⌄
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        
+    }
+};
+Saved
+Testcase
+1
+2
+"AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+"AAAAAAAAAAAAA"
+2/8 testcases
+Line 1
+Case 1: s
+Copyright © 2026 LeetCode. All rights reserved.
+
+3.7K
+
+
+112
+Search questions
