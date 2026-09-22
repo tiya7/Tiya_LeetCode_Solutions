5eeb12610128e517bfc5b088418589f1d6807c8f
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-22 23:03:16 +0530

    Update 112) House Robber

--- LeetCode_Solutions/Medium/112) House Robber.cpp
+++ LeetCode_Solutions/Medium/112) House Robber.cpp
@@ -91,3 +91,18 @@ Copyright © 2026 LeetCode. All rights reserved.
 
 466
 Search questions
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int prev2 = 0;
+        int prev1 = 0;
+
+        for (int money : nums) {
+            int current = max(prev1, prev2 + money);
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+};
