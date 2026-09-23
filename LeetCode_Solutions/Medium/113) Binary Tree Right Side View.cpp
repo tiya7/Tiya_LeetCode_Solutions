48096c9934403f4978c41f4fbd55c126787aa628
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-23 19:35:08 +0530

    Update 113) Binary Tree Right Side View.

--- LeetCode_Solutions/Medium/113) Binary Tree Right Side View.cpp
+++ LeetCode_Solutions/Medium/113) Binary Tree Right Side View.cpp
@@ -3,4 +3,35 @@ public:
     vector<int> rightSideView(TreeNode* root) {
         
     }
+};class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> ans;
+
+        if (root == NULL)
+            return ans;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int n = q.size();
+
+            for (int i = 0; i < n; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                if (i == n - 1)
+                    ans.push_back(node->val);
+
+                if (node->left != NULL)
+                    q.push(node->left);
+
+                if (node->right != NULL)
+                    q.push(node->right);
+            }
+        }
+
+        return ans;
+    }
 };
