4fc0a5fe1d6077a3e9a50d6156a0de419418a183
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-25 23:32:02 +0530

    Update 115) Bitwise AND of Numbers Range.cpp

--- LeetCode_Solutions/Medium/115) Bitwise AND of Numbers Range.cpp
+++ LeetCode_Solutions/Medium/115) Bitwise AND of Numbers Range.cpp
@@ -1,5 +1,18 @@
 
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int count = 0;
 
+        while (left != right) {
+            left = left >> 1;
+            right = right >> 1;
+            count++;
+        }
+
+        return left << count;
+    }
+};
 Avatar
 TiyaGandhi
 Access all features with our Premium subscription!
