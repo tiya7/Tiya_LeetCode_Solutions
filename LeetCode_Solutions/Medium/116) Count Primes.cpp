f09aea82e8793990382d16af737e3f90edd6cceb
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-26 00:52:57 +0530

    Update 116)Count Primes

--- LeetCode_Solutions/Medium/116) Count Primes.cpp
+++ LeetCode_Solutions/Medium/116) Count Primes.cpp
@@ -3,4 +3,31 @@ public:
     int countPrimes(int n) {
         
     }
+};class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool> prime(n, true);
+
+        if (n <= 2)
+            return 0;
+
+        prime[0] = prime[1] = false;
+
+        for (int i = 2; i * i < n; i++) {
+            if (prime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    prime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (prime[i])
+                count++;
+        }
+
+        return count;
+    }
 };
