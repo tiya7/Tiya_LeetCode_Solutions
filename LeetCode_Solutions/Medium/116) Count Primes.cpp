1f1c198eaf9539a273019dadd00b38adf0f6e496
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-26 00:51:03 +0530

    Count Primes

---
+++ LeetCode_Solutions/Medium/116) Count Primes.cpp
@@ -0,0 +1,93 @@
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+204. Count Primes
+Medium
+Topics
+premium lock icon
+Companies
+Given an integer n, return the number of prime numbers that are strictly less than n.
+
+ 
+
+Example 1:
+
+Input: n = 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+Example 2:
+
+Input: n = 0
+Output: 0
+Example 3:
+
+Input: n = 1
+Output: 0
+ 
+
+Constraints:
+
+0 <= n <= 5 * 106
+ 
+Accepted
+1,362,915/3.8M
+Acceptance Rate
+35.9%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+⌄
+⌄
+class Solution {
+public:
+    int countPrimes(int n) {
+        
+    }
+};
+Saved
+Testcase
+1
+2
+3
+10
+0
+1
+3/8 testcases
+Line 1
+Case 1: n
+Copyright © 2026 LeetCode. All rights reserved.
+
+9K
+
+
+203
+Search questions
