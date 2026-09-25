92907ea63a1d675c3038195463df16d7e966f48a
Tiya Gandhi <144030474+tiya7@users.noreply.github.com>
2026-09-25 23:29:43 +0530

    Bitwise AND of Numbers Range

--- LeetCode_Solutions/Medium/115) Bitwise AND of Numbers Range.cpp
+++ LeetCode_Solutions/Medium/115) Bitwise AND of Numbers Range.cpp
@@ -4,3 +4,101 @@ public:
         
     }
 };
+
+
+Avatar
+TiyaGandhi
+Access all features with our Premium subscription!
+myLists
+My Lists
+notebook
+Notebook
+progress
+Progress
+points
+Points
+Problems
+Discuss
+Contest
+Interview
+Store
+Try New Features
+Orders
+My Playgrounds
+Settings
+Appearance
+Sign Out
+201. Bitwise AND of Numbers Range
+Medium
+Topics
+premium lock icon
+Companies
+Given two integers left and right that represent the range [left, right], return the bitwise AND of all numbers in this range, inclusive.
+
+ 
+
+Example 1:
+
+Input: left = 5, right = 7
+Output: 4
+Example 2:
+
+Input: left = 0, right = 0
+Output: 0
+Example 3:
+
+Input: left = 1, right = 2147483647
+Output: 0
+ 
+
+Constraints:
+
+0 <= left <= right <= 231 - 1
+ 
+Accepted
+543,007/1.1M
+Acceptance Rate
+49.2%
+C++
+
+
+
+
+1
+2
+3
+4
+5
+6
+⌄
+⌄
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        
+    }
+};
+Saved
+Testcase
+1
+2
+3
+4
+5
+6
+5
+7
+0
+0
+1
+2147483647
+3/8 testcases
+Line 1
+Case 1: left
+Copyright © 2026 LeetCode. All rights reserved.
+
+4.3K
+
+
+115
+Search questions
